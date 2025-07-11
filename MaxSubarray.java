5722f8497c9827f49296f4fa6ed2133e6790596f
sswara18 <sonawaneswarup952gmail.com>
2025-07-11 19:18:19 +0530

    Add Max Subarray (Kadane's Algorithm) solution

---
+++ MaxSubarray.java
@@ -0,0 +1,14 @@
+public class MaxSubarray {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // Either extend the current subarray or start new from current element
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
+        }
+
+        return maxSum;
+    }
+}
